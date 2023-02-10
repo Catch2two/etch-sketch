46a8477b078471796241b10dc8ca35841f9d34c0
Josh <118862870+Catch2two@users.noreply.github.com>
2023-02-10 11:49:29 -0500

    Created CSS/JS
    Css: used grid to customize each "box"
    
    JS: Created a function that holds rows and columns based on output.
    ex. makeRows(100, 100) will make 100x100 grid.

---
+++ etch.js
@@ -0,0 +1,20 @@
+/* link to div id 'container' */
+const container = document.getElementById('container');
+
+/* create rows and columns function*/
+function makeRows (rows, columns) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', columns);
+
+ /* set limit of grid to number of rows * number of columns*/ 
+    for (i = 0; i <(rows * columns); i++) {
+        /* Create a new div element with text inside */
+        let cell  = document.createElement('div');
+        cell.innerText = (i + 1);
+        /* visually add makeRows to the page*/
+        container.appendChild(cell).className = "grid-item";
+        console.log(makeRows);
+};
+};
+
+makeRows(16, 16);
