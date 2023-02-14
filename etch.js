461efba42351bc7a44cb93e3880ec5dcaf321cc0
Josh <118862870+Catch2two@users.noreply.github.com>
2023-02-14 12:23:52 -0500

    Update etch.js
    Created a Function (createBoard) which creates a board based on its input size
    createBoard(size)

---
+++ etch.js
@@ -0,0 +1,13 @@
+function createBoard(size) {
+    let board = document.querySelector(".board");
+//This will make 16 Columns(andRows) w/ width of 16
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`
+
+    for (let i = 0; i < 256; i++) {
+     let square = document.createElement("div");
+     square.style.backgroundColor = "blue";
+     board.insertAdjacentElement("beforeEnd" , square);
+}}
+
+createBoard(16);
